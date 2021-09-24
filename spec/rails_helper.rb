84a9df60109069980333832e27c74957dd609798
maxarvid <max.arvid.anderson@gmail.com>
2021-09-24 23:16:13 +0200

    Adds additional testing support through a JSON parser

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,7 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
-
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 
 Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
@@ -20,5 +19,6 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
   config.include FactoryBot::Syntax::Methods
-  config.include Shoulda::Matchers::ActiveRecord, type: :model  
+  config.include Shoulda::Matchers::ActiveRecord, type: :model
+  config.include ResponseJSON
 end
