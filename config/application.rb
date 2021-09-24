711412f92ba4173208abb948c16afbfdff247b52
maxarvid <max.arvid.anderson@gmail.com>
2021-09-24 22:55:28 +0200

    Actually removes the errant code in config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -18,18 +18,7 @@ module UnterChallenge
   class Application < Rails::Application
     config.load_defaults 6.1
     config.api_only = true
-
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*',
-                 headers: any,
-                 methods: %i[get post put delete],
-                 expose: %w[access-token expiry token-type uid client],
-                 max_age: 0
-      end
-    end
-    
+        
     # Disable generation of helpers, assets, routing
     # and specs for helpers, routes, controller and request
 
